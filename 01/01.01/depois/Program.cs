0877cc928dc8f1ac4c2238ff1c6c284d8d5d7a18
Marcelo Oliveira <mclricardo@gmail.com>
2018-11-19 17:22:43 -0200

    1054-CertificacaoCSharpParte7-video1.2-Criando a Classe Campainha

--- 01/01.01/depois/Program.cs
+++ 01/01.01/depois/Program.cs
@@ -2,11 +2,28 @@
 
 namespace _01_01
 {
-    /// < image url="$(ProjectDir)/img1.png"/>
+    /// <image url="$(ProjectDir)/img1.png"/>
     class Program
     {
         static void Main(string[] args)
         {
+            Campainha campainha = new Campainha();
+            campainha.Tocar();
+            Console.ReadKey();
+        }
+    }
+
+    class Campainha
+    {
+        public Action OnCampainhaTocou { get; set; }
+
+        public void Tocar()
+        {
+            if (OnCampainhaTocou != null)
+            {
+                OnCampainhaTocou();
+            }
+            
         }
     }
 }
