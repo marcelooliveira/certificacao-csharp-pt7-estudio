29dbd0516b51a79ad639c9fb6fe551c4a76cf128
Marcelo Oliveira <mclricardo@gmail.com>
2018-11-19 15:57:16 -0200

    .

--- 01/01.01/depois/Program.cs
+++ 01/01.01/depois/Program.cs
@@ -4,42 +4,8 @@ namespace _01_01
 {
     class Program
     {
-        static void CampainhaTocou1()
-        {
-            Console.WriteLine("A campainha tocou.");
-        }
-
-        static void CampainhaTocou2()
-        {
-            Console.WriteLine("A campainha tocou.");
-        }
-
         static void Main(string[] args)
         {
-            Campainha campainha = new Campainha();
-            //campainha.Tocar();
-            //campainha.OnCampainhaTocou = new Action(() => { });
-            campainha.OnCampainhaTocou += CampainhaTocou1;
-            campainha.OnCampainhaTocou += CampainhaTocou2;
-
-            campainha.Tocar();
-            Console.ReadKey();
         }
     }
-
-    class Campainha
-    {
-        public Action OnCampainhaTocou { get; set; }
-
-        public void Tocar()
-        {
-            //if (OnCampainhaTocou != null)
-            //{
-            //    OnCampainhaTocou();
-            //}
-
-            OnCampainhaTocou?.Invoke();
-        }
-    }
-
 }
