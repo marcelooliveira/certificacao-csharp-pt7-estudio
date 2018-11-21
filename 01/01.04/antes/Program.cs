b54cf52b9c9390f389575bdfe58c113b2c46a0e6
marcelooliveira <mclricardo@gmail.com>
2018-11-21 18:40:14 -0200

    .

--- 01/01.04/antes/Program.cs
+++ 01/01.04/antes/Program.cs
@@ -4,12 +4,34 @@ namespace _01_04
 {
     class Program
     {
-        static Func<int, int, int> adicionar = (a, b) => a + b;
-        static Action<string> logMensagem = (mensagem) => Console.WriteLine(mensagem);
-        static Predicate<int> divisivelPor3 = (i) => i % 3 == 0;
+        delegate int Operacao(int a, int b);
 
-        static void Main()
+        static void Main(string[] args)
         {
+            int a = 3;
+            int b = 2;
+
+            Operacao operacao = Somar;
+
+            Console.WriteLine(operacao(a, b));
+
+            operacao = Subtrair;
+
+            Console.WriteLine(operacao(a, b));
+
+            Console.ReadKey();
+        }
+
+        static int Somar(int a, int b)
+        {
+            Console.WriteLine($"A operação Somar foi chamada com a = {a} e b = {b}");
+            return a + b;
+        }
+
+        static int Subtrair(int a, int b)
+        {
+            Console.WriteLine($"A operação Subtrair foi chamada com a = {a} e b = {b}");
+            return a - b;
         }
     }
 }
--- 01/01.04/depois/Program.cs
+++ 01/01.04/depois/Program.cs
@@ -4,19 +4,34 @@ namespace _01_04
 {
     class Program
     {
-        static Func<int, int, int> adicionar = (a, b) => a + b;
-        static Action<string> logMensagem = (mensagem) => Console.WriteLine(mensagem);
-        static Predicate<int> divisivelPor3 = (i) => i % 3 == 0;
+        delegate int Operacao(int a, int b);
 
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine($"adicionar(2, 3): {adicionar(2, 3)}");
-            Console.WriteLine($"logMensagem('esta é uma Action')");
-            logMensagem("esta é uma Action");
-            var numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var divisiveis = Array.FindAll(numeros, divisivelPor3);
-            Console.WriteLine("Divisíveis por 3: " + string.Join(',', divisiveis));
+            int a = 3;
+            int b = 2;
+
+            Operacao operacao = Somar;
+
+            Console.WriteLine(operacao(a, b));
+
+            operacao = Subtrair;
+
+            Console.WriteLine(operacao(a, b));
+
             Console.ReadKey();
+        }
+
+        static int Somar(int a, int b)
+        {
+            Console.WriteLine($"A operação Somar foi chamada com a = {a} e b = {b}");
+            return a + b;
+        }
+
+        static int Subtrair(int a, int b)
+        {
+            Console.WriteLine($"A operação Subtrair foi chamada com a = {a} e b = {b}");
+            return a - b;
         }
     }
 }
