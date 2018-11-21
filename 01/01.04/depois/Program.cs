8d606409c8beca656f1c3f85546d665325f681c9
marcelooliveira <mclricardo@gmail.com>
2018-11-21 19:08:46 -0200

    1054-CertificacaoCSharpParte7-video2.2-Expressão Lambda e Métodos Anônimos.trec

--- 01/01.04/depois/Program.cs
+++ 01/01.04/depois/Program.cs
@@ -4,8 +4,14 @@ namespace _01_04
 {
     class Program
     {
+        delegate int Operacao(int a, int b);
+
         static void Main(string[] args)
         {
+            Operacao operacao = (x, y) => x + y;
+            Console.WriteLine(operacao(3, 2));
+
+            Console.ReadKey();
         }
     }
 }
