f6943a83eb776cbb4a0f09cb6ec918944eb1933d
marcelooliveira <mclricardo@gmail.com>
2018-11-21 16:10:48 -0200

    1054-CertificacaoCSharpParte7-video1.6-Acessando Manipuladores de Eventos.trec

--- 01/01.02/depois/Program.cs
+++ 01/01.02/depois/Program.cs
@@ -6,15 +6,24 @@ namespace _01_02
     {
         static void Main(string[] args)
         {
-            Campainha campainha = new Campainha();
-            campainha.OnCampainhaTocou += CampainhaTocou1;
-            campainha.OnCampainhaTocou += CampainhaTocou2;
-            Console.WriteLine("A campainha será tocada.");
-            campainha.Tocar("101");
+            try
+            {
 
-            campainha.OnCampainhaTocou -= CampainhaTocou1;
-            Console.WriteLine("A campainha será tocada.");
-            campainha.Tocar("202");
+                Campainha campainha = new Campainha();
+                campainha.OnCampainhaTocou += CampainhaTocou1;
+                campainha.OnCampainhaTocou += CampainhaTocou2;
+                Console.WriteLine("A campainha será tocada.");
+                campainha.Tocar("101");
+
+                campainha.OnCampainhaTocou -= CampainhaTocou1;
+                Console.WriteLine("A campainha será tocada.");
+                campainha.Tocar("202");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
 
             Console.ReadKey();
 
@@ -23,10 +32,12 @@ namespace _01_02
         static void CampainhaTocou1(object sender, CampainhaEventArgs args)
         {
             Console.WriteLine("A campainha tocou no apartamento " + args.Apartamento + " .(1)");
+            throw new Exception("Ocorreu um erro em CampainhaTocou1");
         }
         static void CampainhaTocou2(object sender, CampainhaEventArgs args)
         {
             Console.WriteLine("A campainha tocou no apartamento " + args.Apartamento + " .(2)");
+            throw new Exception("Ocorreu um erro em CampainhaTocou2");
         }
     }
 
@@ -36,10 +47,19 @@ namespace _01_02
 
         public void Tocar(string apartamento)
         {
-            if (OnCampainhaTocou != null)
+            foreach (var manipulador in OnCampainhaTocou.GetInvocationList())
             {
-                OnCampainhaTocou(this, new CampainhaEventArgs(apartamento));
+                try
+                {
+                    manipulador.DynamicInvoke(this, new CampainhaEventArgs(apartamento));
+                }
+                catch (Exception e)
+                {
+
+                }
             }
+            
+
 
         }
     }
