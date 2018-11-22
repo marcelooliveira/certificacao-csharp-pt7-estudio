a46f83bba8d4f87dde1e3322693c092b4bf9fa7f
Marcelo Oliveira <mclricardo@gmail.com>
2018-11-22 13:13:12 -0200

    1054-CertificacaoCSharpParte7-video2.3-Delegado Func e Delegado Action

--- 01/01.04/depois/Program.cs
+++ 01/01.04/depois/Program.cs
@@ -8,8 +8,19 @@ namespace _01_04
 
         static void Main(string[] args)
         {
-            Operacao operacao = (x, y) => x + y;
+            Operacao operacao = (x, y) => x + y; //expressão lambda
             Console.WriteLine(operacao(3, 2));
+
+            Func<int, int, int> somar = (x, y) => x + y;
+            Console.WriteLine(somar(3, 2));
+
+            Action<string> imprimirMensagem 
+                = (mensagem) => 
+                {
+                    Console.WriteLine(mensagem);
+                };
+
+            imprimirMensagem("Olá, Alura!");
 
             Console.ReadKey();
         }
