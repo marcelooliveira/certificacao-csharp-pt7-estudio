887de6661bc0cfde4eec492fafa4274972dd337c
Marcelo Oliveira <mclricardo@gmail.com>
2018-11-22 13:52:08 -0200

    1054-CertificacaoCSharpParte7-video2.4-Criando e Consumindo Predicados.trec

--- 01/01.04/depois/Program.cs
+++ 01/01.04/depois/Program.cs
@@ -22,6 +22,16 @@ namespace _01_04
 
             imprimirMensagem("Olá, Alura!");
 
+            var numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Números divisíveis por 3:");
+            Predicate<int> divisivelPor3 = (numero) => numero % 3 == 0;
+            var divisiveis = Array.FindAll(numeros, divisivelPor3);
+
+            foreach (var numero in divisiveis)
+            {
+                Console.WriteLine(numero);
+            }
+
             Console.ReadKey();
         }
     }
