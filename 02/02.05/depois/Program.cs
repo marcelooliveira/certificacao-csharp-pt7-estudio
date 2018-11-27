53b19f0ede50c1c6330bdc6eeb3f41eaff53bbad
Marcelo Oliveira <mclricardo@gmail.com>
2018-11-27 18:57:02 -0200

    1054-CertificacaoCSharpParte7-video6.1-LINQ to XML com Sintaxe de Método.trec

--- 02/02.05/depois/Program.cs
+++ 02/02.05/depois/Program.cs
@@ -39,6 +39,31 @@ namespace _02_05
                 Console.WriteLine(item.Element("Titulo").FirstNode);
             }
 
+            Console.WriteLine();
+
+            IEnumerable<XElement> consulta2 =
+            from f in documento.Descendants("Filme")
+            where (string)f.Element("Diretor") == "James Cameron"
+            select f;
+
+            foreach (var item in consulta2)
+            {
+                Console.WriteLine((string)item.Element("Diretor"));
+                Console.WriteLine((string)item.Element("Titulo"));
+            }
+
+            Console.WriteLine();
+
+            IEnumerable<XElement> consulta3 =
+            documento.Descendants("Filme")
+            .Where(elemento => (string)elemento.Element("Diretor") == "James Cameron");
+
+            foreach (var item in consulta3)
+            {
+                Console.WriteLine((string)item.Element("Diretor"));
+                Console.WriteLine((string)item.Element("Titulo"));
+            }
+
             Console.ReadKey();
         }
     }
