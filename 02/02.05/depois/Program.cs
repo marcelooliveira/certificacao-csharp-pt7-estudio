c95f79f0750bf12c6ecc083062d7bbd1484473f6
Marcelo Oliveira <mclricardo@gmail.com>
2018-11-27 17:04:33 -0200

    .

--- 02/02.05/depois/Program.cs
+++ 02/02.05/depois/Program.cs
@@ -3,159 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
-namespace _02_05
+namespace _02_06
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Diretor> diretores = GetDiretores();
-            List<Filme> filmes = GetFilmes();
+            string xmlText =
+            "<Filmes>" +
+                "<Filme>" +
+                    "<Diretor>Quentin Tarantino</Diretor>" +
+                    "<Titulo>Pulp Fiction</Titulo>" +
+                    "<Minutos>154</Minutos>" +
+                "</Filme>" +
+                "<Filme>" +
+                    "<Diretor>James Cameron</Diretor>" +
+                    "<Titulo>Avatar</Titulo>" +
+                    "<Minutos>162</Minutos>" +
+                "</Filme>" +
+            "</Filmes>";
 
-            var diretorFilmeQuery =
-            from diretor in diretores
-            where diretor.Nome == "James Cameron"
-            join filme in filmes
-            on diretor.Id equals filme.DiretorId
-            select new
-            {
-                NomeDiretor = diretor.Nome,
-                filme.Titulo
-            };
+            XDocument documentoFilmes = XDocument.Parse(xmlText);
 
-            var diretorFilmeArray = diretorFilmeQuery.ToArray();
-            foreach (var item in diretorFilmeArray)
+            IEnumerable<XElement> filmesSelecionados2 =
+            from filme in documentoFilmes.Descendants("Filme")
+            select filme;
+            foreach (XElement item in filmesSelecionados2)
             {
-                Console.WriteLine($"{item.NomeDiretor,-30}{item.Titulo,-30}");
+                Console.WriteLine("Diretor: {0}, Título: {1} ",
+                    item.Element("Diretor").FirstNode,
+                    item.Element("Titulo").FirstNode);
+            }
+            Console.WriteLine();
+
+
+            filmesSelecionados2 =
+            from filme in documentoFilmes.Descendants("Filme")
+            where (string)filme.Element("Diretor") == "Quentin Tarantino"
+            select filme;
+            foreach (XElement item in filmesSelecionados2)
+            {
+                Console.WriteLine("Diretor: {0}, Título: {1} ",
+                    item.Element("Diretor").FirstNode,
+                    item.Element("Titulo").FirstNode);
+            }
+            Console.WriteLine();
+
+
+            filmesSelecionados2 =
+            from filme in documentoFilmes.Descendants("Filme")
+            .Where(elemento => (string)elemento.Element("Diretor")
+                == "Quentin Tarantino")
+            select filme;
+            foreach (XElement item in filmesSelecionados2)
+            {
+                Console.WriteLine("Diretor: {0}, Título: {1} ",
+                    item.Element("Diretor").FirstNode,
+                    item.Element("Titulo").FirstNode);
+            }
+            Console.WriteLine();
+
+            XElement filmesXML = new XElement("Filmes",
+                new List<XElement>
+                {
+                    new XElement ("Filme" ,
+                        new XElement ("Diretor" , "Steven Spielberg"),
+                        new XElement ("Titulo" , "A Lista de Schindler")),
+                    new XElement ("Filme",
+                        new XElement ("Diretor" , "Christopher Nolan"),
+                        new XElement ("Titulo" , "Batman: O Cavaleiro das Trevas"))
+                }
+            );
+
+
+
+
+            filmesSelecionados2 =
+            from filme in documentoFilmes.Descendants("Filme")
+            select filme;
+            foreach (XElement item in filmesSelecionados2)
+            {
+                item.Element("Titulo").FirstNode.ReplaceWith("Novo filme");
+            }
+
+            foreach (XElement item in filmesSelecionados2)
+            {
+                Console.WriteLine("Diretor: {0}, Título: {1} ",
+                    item.Element("Diretor").FirstNode,
+                    item.Element("Titulo").FirstNode);
+            }
+            Console.WriteLine();
+
+
+
+            var filmesSelecionados3 =
+            from filme in documentoFilmes.Descendants("Filme")
+            select filme;
+            foreach (XElement item in filmesSelecionados3)
+            {
+                item.Add(new XElement("Genero", "Drama"));
+            }
+
+            foreach (XElement item in filmesSelecionados3)
+            {
+                Console.WriteLine("Diretor: {0}, Título: {1}, Gênero: {2} ",
+                    item.Element("Diretor").FirstNode,
+                    item.Element("Titulo").FirstNode,
+                    item.Element("Genero").FirstNode);
             }
             Console.WriteLine();
 
 
             Console.ReadKey();
         }
-
-        private static void Imprimir(IEnumerable<Filme> filmes)
-        {
-            Console.WriteLine($"{"Título",-40}{"Diretor",-20}{"Ano",4}");
-            Console.WriteLine(new string('=', 64));
-            foreach (var filme in filmes)
-            {
-                Console.WriteLine($"{filme.Titulo,-40}{filme.Diretor.Nome,-20}{filme.Ano}");
-            }
-            Console.WriteLine();
-        }
-
-        private static void Imprimir(IEnumerable<FilmeResumido> filmes)
-        {
-            Console.WriteLine($"{"Título",-40}{"Diretor",-20}");
-            Console.WriteLine(new string('=', 60));
-            foreach (var filme in filmes)
-            {
-                Console.WriteLine($"{filme.Titulo,-40}{filme.NomeDiretor,-20}");
-            }
-            Console.WriteLine();
-        }
-
-        private static List<Diretor> GetDiretores()
-        {
-            return new List<Diretor>
-            {
-                new Diretor { Id = 1, Nome = "Quentin Tarantino" },
-                new Diretor { Id = 2, Nome = "James Cameron" },
-                new Diretor { Id = 3, Nome = "Tim Burton" }
-            };
-        }
-
-        private static List<Filme> GetFilmes()
-        {
-            return new List<Filme> {
-                new Filme {
-                    DiretorId = 1,
-                    Diretor = new Diretor { Nome = "Quentin Tarantino" },
-                    Titulo = "Pulp Fiction",
-                    Ano = 1994,
-                    Minutos = 2 * 60 + 34
-                },
-                new Filme {
-                    DiretorId = 1,
-                    Diretor = new Diretor { Nome = "Quentin Tarantino" },
-                    Titulo = "Django Livre",
-                    Ano = 2012,
-                    Minutos = 2 * 60 + 45
-                },
-                new Filme {
-                    DiretorId = 1,
-                    Diretor = new Diretor { Nome = "Quentin Tarantino" },
-                    Titulo = "Kill Bill Volume 1",
-                    Ano = 2003,
-                    Minutos = 1 * 60 + 51
-                },
-
-                new Filme {
-                    DiretorId = 2,
-                    Diretor = new Diretor { Nome = "James Cameron" },
-                    Titulo = "Avatar",
-                    Ano = 2009,
-                    Minutos = 2 * 60 + 42
-                },
-                new Filme {
-                    DiretorId = 2,
-                    Diretor = new Diretor { Nome = "James Cameron" },
-                    Titulo = "Titanic",
-                    Ano = 1997,
-                    Minutos = 3 * 60 + 14
-                },
-                new Filme {
-                    DiretorId = 2,
-                    Diretor = new Diretor { Nome = "James Cameron" },
-                    Titulo = "O Exterminador do Futuro",
-                    Ano = 1984,
-                    Minutos = 1 * 60 + 47
-                },
-
-                new Filme {
-                    DiretorId = 3,
-                    Diretor = new Diretor { Nome = "Tim Burton" },
-                    Titulo = "O Estranho Mundo de Jack",
-                    Ano = 1993,
-                    Minutos = 1 * 60 + 16
-                },
-                new Filme {
-                    DiretorId = 3,
-                    Diretor = new Diretor { Nome = "Tim Burton" },
-                    Titulo = "Alice no País das Maravilhas",
-                    Ano = 2010,
-                    Minutos = 1 * 60 + 48
-                },
-                new Filme {
-                    DiretorId = 3,
-                    Diretor = new Diretor { Nome = "Tim Burton" },
-                    Titulo = "A Noiva Cadáver",
-                    Ano = 2005,
-                    Minutos = 1 * 60 + 17
-                }
-            };
-        }
-    }
-
-    class Diretor
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-    }
-
-    class Filme
-    {
-        public int DiretorId { get; set; }
-        public Diretor Diretor { get; set; }
-        public string Titulo { get; set; }
-        public int Ano { get; set; }
-        public int Minutos { get; set; }
-    }
-
-    class FilmeResumido
-    {
-        public string NomeDiretor { get; set; }
-        public string Titulo { get; set; }
     }
 }
